ae82be8bb27b3ff76fa53b54ef3afd03bfb6e10b
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2023-11-03 20:08:11 -0700

    Update device_ally.rs
    35w limit test

--- src/devices/device_ally.rs
+++ src/devices/device_ally.rs
@@ -15,7 +15,7 @@ pub struct DeviceAlly {
 impl DeviceAlly {
     pub fn new() -> Self {
         DeviceAlly {
-            device: DeviceGeneric::new(45, 800,2700),
+            device: DeviceGeneric::new(35, 800,2700),
         }
     }
 }
